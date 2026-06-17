63a0c766076386175d0ea82762513b01d8224c12
Syfur007 <syfur.cse8.bu@gmail.com>
2026-06-18 01:58:27 +0600

    Refactor code structure for improved readability and maintainability

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <cstdio>
+#include <GL/gl.h>
+#include <GL/glut.h>
+#include <math.h>
+#include <iostream>
+#include <string.h>
+#include <stdlib.h>
+#include <cmath>
+
+#include "scene.inc"
+
+void myInit(void)
+{
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(0.0, 1200.0, 0.0, 600.0);
+}
+
+int main(int argc, char **argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(1200, 600);
+    glutInitWindowPosition(100, 150);
+    glutCreateWindow("A Modern Sub-City");
+    glMatrixMode(GL_PROJECTION);
+    myInit();
+    glutTimerFunc(16, update1, 0);
+    glutDisplayFunc(Day_Scn);
+    glutKeyboardFunc(keyboard1);
+    glutSpecialFunc(keypressed);
+
+    glutMainLoop();
+}
